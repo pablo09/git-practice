0a86e068bd1de37a197960586dc2a4ccdf185d0c
pzeszko <pzeszko@gmail.com>
2017-05-16 14:59:35 +0200

    Added print help

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,16 @@
 public class Main {
     public static void main(String[] args) {
         printInfo();
+        printHelp();
     }
 
     public static void printInfo() {
         System.out.println("----------------------------");
         System.out.println("This is Git practice project.");
         System.out.println("----------------------------");
+    }
+
+    public static void printHelp() {
+        System.out.println("If you need help contact me: pzeszko@gmail.com");
     }
 }
