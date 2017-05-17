592af6c06c485be3bd3fe425f704d3c4e4f1f299
pzeszko <pzeszko@gmail.com>
2017-05-17 10:26:52 +0200

    Master commit

--- src/main/java/newdir/Main.java
+++ src/main/java/newdir/Main.java
@@ -3,7 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to my app");
+        System.out.println("Welcome to my app/. MASTER");
         printHelp();
         printHelp();
         stopServer();
