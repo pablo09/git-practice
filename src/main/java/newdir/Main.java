d32c6f468f78bb407b6fa20ea0e9eaa09d92f050
pzeszko <pzeszko@gmail.com>
2017-05-17 10:35:05 +0200

    Removed trash

--- src/main/java/newdir/Main.java
+++ src/main/java/newdir/Main.java
@@ -37,36 +37,5 @@ public class Main {
         System.out.println("Closing streams...");
     }
 
-    public static void test1() {
-        System.out.println("Test1 in progress...");
-    }
 
-    public static void test2() {
-        System.out.println("Test2 in progress");
-    }
-
-    public static void test3() {
-        System.out.println("Test in progress");
-    }
-
-    public static void test4() {
-        System.out.println("Test in progress");
-    }
-
-    public static void test5() {
-        System.out.println("Test in progress");
-    }
-
-    public static void test6() {
-        System.out.println("Test in progress");
-
-    }
-
-    public static void test() {
-        System.out.println("Test in progress");
-    }
-
-    public static void test() {
-        System.out.println("Test in progress");
-    }
 }
